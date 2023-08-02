8d865566ac7c3554c761d5bf1bc4619df670b1ed
Susobhan Das <susobhandas009@gmail.com>
2023-08-02 11:09:59 +0530

    added font support, drift is still monitored

--- styles.js
+++ styles.js
@@ -1,7 +1,9 @@
-const colors = document.getElementsByClassName("setting-option-radio");
+const inputs = document.getElementsByClassName("setting-option-radio");
 const colorLabels = document.getElementsByClassName("color-label");
 
-for (let colorInput of colors) {
+inputs[0].checked = true;
+inputs[3].checked = true;
+for (let colorInput of inputs) {
     colorInput.addEventListener('change', (e => {
         const { name, value } = e.target;
         document.documentElement.style.setProperty(name, value);
